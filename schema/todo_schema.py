2fb8e7ee6e8185c4fdfddcacf464c57666406c42
rhoda-lee <rhodalee.dev@gmail.com>
2025-12-12 18:56:24 +0000

    Added subtask schema: base class, subtask_create class, subtask_update class and subtaskresponse class

--- schema/todo_schema.py
+++ schema/todo_schema.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 # My input schema for creating or updating todos
 
-
+# ---- TODO SCHEMA ----
 class TodoBase(BaseModel):
     title: str
     description: str | None = None
@@ -22,10 +22,32 @@ class TodoUpdate(TodoBase):
     due_date: datetime | None = None
 
 
+# ---- SUBTASKS SCHEMA ----
+class SubtaskBase(BaseModel):
+    title: str
+    is_complete: bool = False
+
+class SubtaskCreate(SubtaskBase):
+    pass
+
+class SubtaskUpdate(SubtaskBase):
+    title: str | None = None
+    is_complete: bool | None = None
+
+class SubtaskResponse(SubtaskBase):
+    id: int
+    todo_id: int
+
+    model_config = {
+        "from_attributes": True
+    }
+
+
 class TodoResponse(TodoBase):
     id: int
     created_at: datetime
     updated_at: datetime | None = None
+    subtasks: list[SubtaskResponse] = []
 
     model_config = {
         "from_attributes": True
